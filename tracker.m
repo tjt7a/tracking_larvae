8477276b7f664c7806aad0f5e702078f7d0f1c1d
Tommy Tracy II <tjt7a@virginia.edu>
2013-09-18 21:28:48 -0400

    Using tracker to combine components

--- tracker.m
+++ tracker.m
@@ -1,7 +1,22 @@
 
 %Definitions of state machine
 count  = 0; % Number of larvae
-larvae = []; % Array of larvae
-%Initialize array of larvae
+larvae = []; % Array of larvae for for first 10 frames
 
-%Iterate through frames    
+%Obtain first 10 frames
+obtaining_frames
+
+for i = 1 : 10
+    file_location = sprintf('individualframes/%3.3d.png', i);
+    I = imread(file_location);
+    track(I); %What is this doing?
+    
+    %larvae[i] = findlarvae(I); %This should theoretically grab larvae values 
+    
+    fakelarva = [100 100];
+    %Iterate through larvae and put a '*' on them
+    rectangle('Position',[(fakelarva(1)-12) (fakelarva(2)-12) 24 24], 'EdgeColor','g');
+    text(fakelarva(1),fakelarva(2),'*','FontSize',50, 'Color', 'r')
+    
+    %The next step is to 
+end
