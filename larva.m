54ab8430ff6fdbc56374f8c34edae8468445c533
Tommy Tracy II <tjt7a@virginia.edu>
2013-09-18 20:45:19 -0400

    flattened the structure of the state machine

--- larva.m
+++ larva.m
@@ -4,9 +4,4 @@ classdef larva
         % Previous location of the larva (2-d location)
         previous_location
     end
-    methods
-        function obj=larva(location)
-            obj.previous_location = location;
-        end
-    end
 end
--- tracker.m
+++ tracker.m
@@ -1,27 +1,7 @@
-% Class definition for the tracker state machine
-classdef tracker
-    
-    %Definitions of state machine
-    properties(GetAccess = public, SetAccess = public)
-        count  = 0; % Number of larvae
-        larvae %= [larva([])]; % Array of larvae
-    end
-    
-    %Methods of state machine
-    methods
-        %Constructor for the tracker class; initializes count to 0
-        function obj=tracker()
-            obj.count = 0;
-        end
-        
-        function obj = set_count(obj, count)
-            obj.count = count;
-        end
 
-        %Creates a new larvae at location <location> and adds it to the larvae array
-        function obj = add_larva(obj, location)
-            obj.count = 10;
-            %obj.larvae(obj.count) = larva(location);
-        end
-    end
-end
+%Definitions of state machine
+count  = 0; % Number of larvae
+larvae = []; % Array of larvae
+%Initialize array of larvae
+
+%Iterate through frames    
