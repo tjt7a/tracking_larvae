cd45dc4ecb88dc9e14944cc2346261f1c7e8c858
aep124 <alex.pape@virginia.edu>
2013-09-23 13:21:21 -0400

    Update addredbox.m

--- addredbox.m
+++ addredbox.m
@@ -1,50 +1,49 @@
-function [ outputI ] = addredbox( inputI, pos )
-%ADDMARKER adds a shape at a specified position in an image
+function [ outputI ] = addredbox( inputI, x, y )
+%ADDREDBOX adds a red box at a specified position in an image
 %   Input variable:
-%       inputI = image to which to add marker 
-%           (can be 2-d or 3-d, any  bit depth)
-%       pos = x,y coordinates at which to add marker
+%       inputI = image to which to add box 
+%           (can be 2-d or 3-d, but must be uint8)
+%       pos = x,y coordinates at which to add box
 %   Output variable:
-%       outputI = image with marker added
-%           (3-d, same bit depth as input image)
+%       outputI = image with box added
+%           (3-d, also uint8)
 
 % define constants:
-x = pos(1); y = pos(2);
 k = 25; % halfwidth
 t = 5; % thickness
 
 % define outside and inside of box:
-x_out = x-k:x+k;
-y_out = y-k:y+k;
-x_in  = x-k+t:x+k-t;
-y_in  = y-k+t:y+k-t;
+[H, W, ~] = size(inputI);
+x_out = max(1, x-k):min(W, x+k);
+y_out = max(1, y-k):min(H, y+k);
+x_in  = max(1, x-k+t):min(W, x+k-t);
+y_in  = max(1, y-k+t):min(H, y+k-t);
 
 % if image is 2-d, duplicate to form third dimension:
 [H, W, T] = size(inputI);
 if T==1
-    outputI = zeros(H, W, 3);
+    outputI = uint8(zeros(H, W, 3));
     outputI(:,:,1) = inputI;
     outputI(:,:,2) = inputI;
-    outputI(:,:,2) = inputI;
+    outputI(:,:,3) = inputI;
 else
     outputI = inputI;
 end
 
 % define mask to erase bits within box:
 erase_mask = false(H,W,3);
-erase_mask(x_out,y_out,:) = true;
-erase_mask(x_in,y_in,:) = false;
+erase_mask(y_out,x_out,:) = true;
+erase_mask(y_in,x_in,:) = false;
 
 % define mask to set layer 1 (red) to 1:
 draw_mask = false(H,W,3);
-draw_mask(x_out,y_out,1) = true;
-draw_mask(x_in,y_in,1) = false;
+draw_mask(y_out,x_out,1) = true;
+draw_mask(y_in,x_in,1) = false;
 
-% because the input could have any depth, just set layer 1 of the box
 % to the highest value anywhere in the image
-maxintensity = max(max(max(inputI)));
+maxintensity = 255;
 outputI(erase_mask) = 0;
-outputI(draw_mask) = maxintensity;
+outputI(draw_mask) = 255;
 
 end
 
